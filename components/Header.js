c1dad1469c6df707f07b204c4672c6ccf2192c17
rowend36 <rowendduke36@gmail.com>
2023-08-18 10:00:27 +0100

    filled

---
+++ components/Header.js
@@ -0,0 +1,5 @@
+import Typography from "@mui/material/Typography";
+export default function Header(){
+    return <Typography variant="h1">CSMS</Typogrphy>
+    <Typography variant="body2">Comprehensive School Management Software</Typography>
+}
